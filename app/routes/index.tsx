280829071d0592f7e76b173013ae445099b34c1b
byronPantoja <byronpantoja@gmail.com>
2022-10-13 05:07:48 +0800

    updating landing page - adding contact and bio

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,51 +1,268 @@
-import { Link } from "@remix-run/react";
-
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
 import { useOptionalUser } from "~/utils";
-
+const profile = {
+  name: "Byron Pantoja",
+  email: "byronpantoja@gmail.com",
+  avatar:
+    "https://res.cloudinary.com/db7p5kwzh/image/upload/v1665604595/byronpantoja.com/byron-warm_gjmkkb.jpg",
+  backgroundImage:
+    "https://res.cloudinary.com/db7p5kwzh/image/upload/v1665604619/byronpantoja.com/833_pgfl5z.png",
+  fields: [
+    ["Email", "byronpantoja@gmail.com"],
+    ["Location", "Davao City, Philippines"],
+  ],
+};
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-
-        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <h2 className="text-base font-semibold text-gray-500 tracking-wide uppercase">
-              Hi, my name is
-            </h2>
-            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-              Byron Pantoja.
-            </p>
-            <div className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-              {/* eslint-disable-next-line react/no-unescaped-entities */}
-              <p>I'm an aspiring Fullstack developer.</p>
+    <main className="relative min-h-screen bg-white">
+      <div>
+        <div>
+          <img
+            className="h-32 w-full object-cover lg:h-48"
+            src={profile.backgroundImage}
+            alt=""
+          />
+        </div>
+        <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+          <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+            <div className="flex">
+              <img
+                className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+                src={profile.avatar}
+                alt=""
+              />
+            </div>
+            <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+              <div className="mt-6 min-w-0 flex-1 sm:hidden md:block">
+                <h1 className="truncate text-2xl font-bold text-gray-900">
+                  {profile.name}
+                </h1>
+                <span className="truncate text-xs font-bold text-gray-800">
+                  Tech Enthusiast | Communication Design | Agile Padawan
+                </span>
+              </div>
             </div>
           </div>
-          {/*<div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">*/}
-          {/*  {user ? (*/}
-          {/*    <Link*/}
-          {/*      to="/notes"*/}
-          {/*      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"*/}
-          {/*    >*/}
-          {/*      View Notes for {user.email}*/}
-          {/*    </Link>*/}
-          {/*  ) : (*/}
-          {/*    <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">*/}
-          {/*      <Link*/}
-          {/*        to="/join"*/}
-          {/*        className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"*/}
-          {/*      >*/}
-          {/*        Sign up*/}
-          {/*      </Link>*/}
-          {/*      <Link*/}
-          {/*        to="/login"*/}
-          {/*        className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600  "*/}
-          {/*      >*/}
-          {/*        Log In*/}
-          {/*      </Link>*/}
-          {/*    </div>*/}
-          {/*  )}*/}
-          {/*</div>*/}
+          <div className="mt-6 hidden min-w-0 flex-1 sm:block md:hidden">
+            <h1 className="truncate text-2xl font-bold text-gray-900">
+              {profile.name}
+            </h1>
+          </div>
         </div>
+      </div>
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="mt-3 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
+            Hello, my name is Byron Pantoja.
+          </p>
+          <p className="mx-auto mt-5 max-w-5xl pt-5 text-xl text-gray-500">
+            I'm tech enthusiast based in Davao City. I'm fascinated with
+            communication design and human behaivor. I'm a father, a husband,
+            and a friend. And I'm always looking to improve. I'll write about my
+            journey as a human and things I'm learning here.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div className="mx-auto max-w-7xl py-10 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="relative bg-white shadow-xl">
+            <h2 className="sr-only">Contact us</h2>
+
+            <div className="grid grid-cols-1">
+              {/* Contact information */}
+              <div className="relative overflow-hidden bg-[#f1807b] py-10 px-6 sm:px-10 xl:p-12">
+                <div
+                  className="pointer-events-none absolute inset-0 sm:hidden"
+                  aria-hidden="true"
+                >
+                  <svg
+                    className="absolute inset-0 h-full w-full"
+                    width={343}
+                    height={388}
+                    viewBox="0 0 343 388"
+                    fill="none"
+                    preserveAspectRatio="xMidYMid slice"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
+                      fill="url(#linear1)"
+                      fillOpacity=".1"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="linear1"
+                        x1="254.553"
+                        y1="107.554"
+                        x2="961.66"
+                        y2="814.66"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="#fff" />
+                        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+                <div
+                  className="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 sm:block lg:hidden"
+                  aria-hidden="true"
+                >
+                  <svg
+                    className="absolute inset-0 h-full w-full"
+                    width={359}
+                    height={339}
+                    viewBox="0 0 359 339"
+                    fill="none"
+                    preserveAspectRatio="xMidYMid slice"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
+                      fill="url(#linear2)"
+                      fillOpacity=".1"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="linear2"
+                        x1="192.553"
+                        y1="28.553"
+                        x2="899.66"
+                        y2="735.66"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="#fff" />
+                        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+                <div
+                  className="pointer-events-none absolute top-0 right-0 bottom-0 hidden w-1/2 lg:block"
+                  aria-hidden="true"
+                >
+                  <svg
+                    className="absolute inset-0 h-full w-full"
+                    width={160}
+                    height={678}
+                    viewBox="0 0 160 678"
+                    fill="none"
+                    preserveAspectRatio="xMidYMid slice"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
+                      fill="url(#linear3)"
+                      fillOpacity=".1"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="linear3"
+                        x1="192.553"
+                        y1="325.553"
+                        x2="899.66"
+                        y2="1032.66"
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="#fff" />
+                        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                </div>
+                <h3 className="text-lg font-medium text-white">
+                  Contact information
+                </h3>
+                <p className="mt-6 max-w-3xl text-base text-indigo-50">
+                  This website is a work in progress. If you have any questions,
+                  please feel free to reach out to me:
+                </p>
+                <dl className="mt-8 space-y-6">
+                  <dt>
+                    <span className="sr-only">Email</span>
+                  </dt>
+                  <dd className="flex text-base text-indigo-50">
+                    <EnvelopeIcon
+                      className="h-6 w-6 flex-shrink-0 text-indigo-200"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-3">byronpantoja@gmail.com</span>
+                  </dd>
+                </dl>
+                <ul role="list" className="mt-8 flex space-x-12">
+                  <li>
+                    <a
+                      className="text-indigo-200 hover:text-indigo-100"
+                      href="https://www.facebook.com/dbp833"
+                    >
+                      <span className="sr-only">Facebook</span>
+                      <svg
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-indigo-200 hover:text-indigo-100"
+                      href="https://github.com/byronPantoja"
+                    >
+                      <span className="sr-only">GitHub</span>
+                      <svg
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M11.999 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.386.6.11.819-.26.819-.578 0-.284-.01-1.04-.017-2.04-3.337.724-4.042-1.61-4.042-1.61-.545-1.386-1.332-1.755-1.332-1.755-1.09-.744.082-.73.082-.73 1.205.086 1.838 1.238 1.838 1.238 1.07 1.833 2.81 1.304 3.493.996.109-.775.419-1.303.762-1.603C7.145 17 4.343 15.97 4.343 11.373c0-1.31.468-2.382 1.236-3.22-.124-.304-.536-1.524.118-3.176 0 0 1.007-.323 3.3 1.23.956-.266 1.983-.4 3.003-.404 1.02.005 2.046.138 3.005.404 2.29-1.553 3.296-1.23 3.296-1.23.655 1.652.243 2.872.12 3.176.77.838 1.233 1.91 1.233 3.22 0 4.61-2.806 5.624-5.478 5.921.43.37.814 1.103.814 2.223 0 1.603-.015 2.898-.015 3.291 0 .321.217.695.825.578C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12.001-12"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-indigo-200 hover:text-indigo-100"
+                      href="https://twitter.com/dbp_byron"
+                    >
+                      <span className="sr-only">Twitter</span>
+                      <svg
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M7.548 22.501c9.056 0 14.01-7.503 14.01-14.01 0-.213 0-.425-.015-.636A10.02 10.02 0 0024 5.305a9.828 9.828 0 01-2.828.776 4.94 4.94 0 002.165-2.724 9.867 9.867 0 01-3.127 1.195 4.929 4.929 0 00-8.391 4.491A13.98 13.98 0 011.67 3.9a4.928 4.928 0 001.525 6.573A4.887 4.887 0 01.96 9.855v.063a4.926 4.926 0 003.95 4.827 4.917 4.917 0 01-2.223.084 4.93 4.93 0 004.6 3.42A9.88 9.88 0 010 20.289a13.941 13.941 0 007.548 2.209"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
